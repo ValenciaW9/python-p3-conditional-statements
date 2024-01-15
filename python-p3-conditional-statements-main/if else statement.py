6f3af6e905a23ddc328c028812b0b0750b7c6bb8
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-14 19:12:16 -0500

    python-p3-conditional-statements
    python-p3-conditional-statements

---
+++ python-p3-conditional-statements-main/if else statement.py
@@ -0,0 +1,13 @@
+# Python
+dog = "cuddly"
+
+if dog == "hungry":
+    owner = "Refilling food bowl ."
+elif dog == "thirsty":
+    owner = "Refilling water bowl."
+elif dog == "playful":
+    owner = "Playing tug-or-war."
+elif dog == "cuddly":
+    owner = "Snuggling."
+else:
+    owner = "Reading newspaper."
---
+++ python-p3-conditional-statements-main/if else statements.js
@@ -0,0 +1,15 @@
+// JavaScript
+let dog = "cuddly";
+let owner;
+
+if (dog === "hungry") {
+  owner = "Refilling food bowl.";
+} else if (dog === "thirsty") {
+  owner = "Refilling water bowl.";
+} else if (dog === "playful") {
+  owner = "Playing tug-of-war.";
+} else if (dog === "cuddly") {
+  owner = "Snuggling.";
+} else {
+  owner = "Reading newspaper.";
+}
